457b5bbc67dccde859eb6d9e7a5acf71b74c9e94
jackimarques <jacki.marques@hotmail.com>
2025-03-13 00:09:07 -0300

    s

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,18 +5,20 @@ interface HeaderProps {
 function Header({ setMenuOpen }: HeaderProps) {
   return (
     <header
-      className="flex items-center justify-between bg-blue-500 text-white p-2 h-[200px] w-full"
-      style={{
-        backgroundImage: 'url("/imagens/im1.png")', // Caminho correto da imagem
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center center',
-      }}
-    >
-      <button className="text-2xl" onClick={() => setMenuOpen(true)}>☰</button>
-      <h1 className="text-3xl">MATRACA</h1>
-      <button className="text-2xl">👤</button>
-    </header>
+  className="relative flex items-center justify-between bg-blue-500 text-white p-2 h-[200px] w-full"
+  style={{
+    backgroundImage: 'url("/imagens/im1.png")',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+  }}
+>
+  <div className="absolute inset-0 bg-black opacity-40"></div> {/* Overlay semitransparente */}
+  <button className="text-2xl" onClick={() => setMenuOpen(true)}>☰</button>
+  <h1 className="text-3xl absolute left-1/2 transform -translate-x-1/2 text-white">MATRACA</h1>
+  <button className="text-2xl">👤</button>
+</header>
+
   );
 }
 
