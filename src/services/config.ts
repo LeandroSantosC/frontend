c16c84f3bc9507140f5f3a50607a3ea5154b23e4
JoaoVASF <joaovasferreira@hotmail.com>
2025-03-28 10:05:33 -0300

    Sla

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,13 +1,14 @@
-/**********************************************/
-/******CONFIGURAÇÕES DE DESENVOLVIMENTO********/
+//
+//CONFIGURAÇÕES DE DESENVOLVIMENTO/
 //  URL BACKEND
 const configURL = true
 //  true: usando o Koeyb como backend
 //  false: usando localhost como backend
-/**********************************************/
+/**/
 
 const config = {
     url: configURL ? "https://wasteful-rochell-fatec-aa30349c.koyeb.app" : "http://localhost:8080",
 }
 
 export default config
+
